c432e0a61e770d22cbd54b15033fd35a09003761
VladyslavVagin <vladtwince@gmail.com>
2024-08-19 10:40:27 +0200

    start project

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import { Inter as FontSans } from "next/font/google";
+import { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "LiveDocs is a go-to collaborative docs editor for teams.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
