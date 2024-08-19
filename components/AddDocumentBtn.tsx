9a41211dafbcec6d08c48bd9e3e00323c03ee1f7
VladyslavVagin <vladtwince@gmail.com>
2024-08-19 20:03:15 +0200

    add update

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -1,10 +1,21 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { createDocument } from "@/lib/actions/room.actions";
+import { useRouter } from "next/navigation";
 
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
-  const addDocumentHandler = async () => {};
+  const router = useRouter();
+
+  const addDocumentHandler = async () => {
+    try {
+      const room = await createDocument({ userId, email });
+      if(room) router.push(`/documents/${room.id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Button
@@ -12,7 +23,12 @@ const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
       onClick={addDocumentHandler}
       className="gradient-blue flex gap-1 shadow-md"
     >
-      <Image src="/assets/icons/add.svg" alt="Add document" width={24} height={24}/>
+      <Image
+        src="/assets/icons/add.svg"
+        alt="Add document"
+        width={24}
+        height={24}
+      />
       <p className="hidden sm:block">Start a blank document</p>
     </Button>
   );
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,9 +1,10 @@
-'use server';
+"use server";
 
 import { nanoid } from "nanoid";
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
 import { parseStringify } from "../utils";
+import { metadata } from '../../app/layout';
 
 export const createDocument = async ({
   userId,
@@ -25,12 +26,51 @@ export const createDocument = async ({
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
-      defaultAccesses: [],
+      defaultAccesses: ["room:write"],
     });
 
-    revalidatePath('/');
+    revalidatePath("/");
     return parseStringify(room);
   } catch (error) {
     console.log(`Error creating document: ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    // if (!hasAccess) {
+    //   throw new Error("You don't have access to this document");
+    // }
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error fetching document: ${error}`);
+  }
+};
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updateRoom = await liveblocks.updateRoom(roomId, { metadata: { title } });
+    revalidatePath(`/documents/${roomId}`);
+    return parseStringify(updateRoom);
+  } catch (error) {
+    console.log(`Error updating document: ${error}`);
+  }
+};
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error getting rooms: ${error}`);
   }
 };
